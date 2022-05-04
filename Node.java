105dd5ac1ec6dbc9e304151c2f3d55054215a6fa
RenDev <34013705+sonich2401@users.noreply.github.com>
2022-05-03 23:41:57 -0700

    Added Initial Files with Basic Functionality
    Node.java:
    Contains all code for a intercetion in a road. This contains the code for the
    path finding algorithm
    
    Connection.java:
    A class that says which nodes are connected to whom
    
    Main.java:
    Used to draw stuff to the screen. Used JavaFX because I don't know of any other graphical libraries so if you want to remove JavaFX I do not mind.
    
    makefile:
    Compiles all code and runs it.
    Make sure to change JAVAFX_PATH to where the JavaFX lib folder is stored

---
+++ Node.java
@@ -0,0 +1,172 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Node{
+       public Node(String name, float x, float y){
+              this.x = x;
+              this.y = y;
+              this.name = name;
+              this.links = new ArrayList<Connection>();
+              this.visited = false;
+              this.qeued = false;
+              this.distance = Float.POSITIVE_INFINITY;
+              this.prev = null;
+       }
+       
+       /***
+         * Gets the distance between this node and the other node
+         * Returns a float
+       ***/
+       public float distanceBetweenNodes(Node other){
+              float xdiff = (other.getX() - this.x);
+              float ydiff = (other.getY() - this.y);
+              
+              return (float)Math.sqrt(xdiff * xdiff + ydiff * ydiff);
+       }
+       
+       public float getX(){
+              return x;
+       }
+       
+       public float getY(){
+              return y;
+       }
+       
+       public String getName(){
+              return this.name;
+       }
+       
+       /***
+         * Internally marks the Node as visited so that it is not processed again
+         * Returns nothing
+       ***/
+       public void markVisted(){
+              this.visited = true;
+       }
+       
+       /***
+         * Internally unmarks the Node as visited so that it is processed again
+         * Returns nothing
+       ***/
+       public void markUnvisited(){
+              this.visited = false;
+       }
+       
+       /***
+         * Tells you if the node has been visited
+         * Returns a boolean
+       ***/
+       public boolean isVisited(){
+              return this.visited;
+       }
+       
+       /***
+         * Tells you if the node is currently waiting to be processed
+         * Returns a boolean
+       ***/
+       private boolean isQeued(){
+              return this.qeued;
+       }
+       
+       /***
+         * Connects this node to a destination node
+         * Takes in the node that you are linking to as well as
+         * the Miles Per Hour (float) that the road is set to
+         * Returns nothing
+       ***/
+       public void AddConnection(Node destination, float mph){
+              //See Connection.java
+              Connection con = new Connection(this, destination, mph);
+       }
+       
+       /***
+         * Return the list of nodes that are connected to this one
+         * Returns a ArrayList
+       ***/
+       public ArrayList<Connection> getLinks(){
+              return this.links;
+       }
+
+       /***
+         * Takes in a destination node and calcluates the shortest distance
+         * from 'this' to 'dest' using the dikjta method.
+         *
+         * Returns a LinkedList<Node> that contains a in-sequence
+         * order of the fastest nodes to traverse
+       ***/
+       public LinkedList<Node> getShortestPath(Node dest){
+              LinkedList<Node> processingQeue = new LinkedList<Node>();
+              
+              this.distance = 0;
+              processingQeue.addFirst(this);
+              
+              //Keep checking nodes until all nodes have been processed
+              while(processingQeue.peek() != null){
+                     Node curNode = processingQeue.pollFirst();
+                     //Unmark this node as a node waiting to be processed
+                     curNode.qeued = false;
+                     
+                     //If we have already processed this node then dont process it again
+                     if(curNode.isVisited()){
+                            continue;
+                     }
+              
+                     //Iterate through all lines connecting this node to another node
+                     for(int i = 0; i < curNode.links.size(); i++){
+                            Connection con = curNode.links.get(i);
+                            //Get the neighboring node to this node
+                            Node neighbor = con.getDest(); 
+                            //Links go both ways so make sure that the link we are viewing is not the current node
+                            if(neighbor == curNode){
+                                   neighbor = con.getSource();
+                            }
+                            
+                            //If this node has not been processed then add it to the qeue
+                            if(neighbor.isVisited() == false){
+                                   //Reset the distance flags from any other time this function was called
+                                   if(neighbor.isQeued() == false && neighbor.isVisited() == false){ //Reset if no other node has touched it yet
+                                          neighbor.distance = Float.POSITIVE_INFINITY;
+                                          neighbor.qeued = true;
+                                          processingQeue.addLast(neighbor); //Add to the end of the qeue so that it is processed last
+                                   }
+                            }
+                            
+                             float new_distance = curNode.distance + con.getCost();
+                     
+                            //If we had taken the route of the curNode then would we walk a shorter distance?
+                            if(new_distance < neighbor.distance){
+                                   //Update the smallest path
+                                   neighbor.distance = new_distance;
+                                   neighbor.prev = curNode;
+                            }
+                            //Loop to next element
+                     }
+                     //All links have been added for processing. Set the current node as known
+                     curNode.markVisted();
+              }
+              
+              //Backtrace to create a list of in-order nodes to destination
+              LinkedList<Node> ret_list = new LinkedList<Node>();
+              Node node = dest;
+              while(node != null){
+                     ret_list.addFirst(node);
+                     node = node.prev;
+              }
+              
+              return ret_list;
+       }
+       
+       private String name;
+       //Position
+       private float x;
+       private float y;
+       
+       //Links to this node
+       private ArrayList<Connection> links;
+       
+       //getShortestPath claims these
+       private boolean visited;
+       private boolean qeued;
+       private float distance;
+       private Node prev;       //Last node that edited the distance
+};
